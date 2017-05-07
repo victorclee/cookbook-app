9fe8ed9b67eec732ac2d364b22fe494f616b9430
victorclee <victorchristopherlee@gmail.com>
2017-05-07 13:02:47 -0500

    index show new create edit update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,47 @@
 class RecipesController < ApplicationController
-  def one_recipe_action
-    @recipe = Recipe.first
-    render 'one_recipe_view.html.erb'
+  def index
+    @recipes = Recipe.all
   end
+
+  def show
+    recipe_id = params[:id]
+    @recipe = Recipe.find_by(id: recipe_id)
+  end
+
+  def new
+  end
+
+  def create
+    recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]    
+                        )
+    recipe.save
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.assign_attributes(
+                            title: params[:title],
+                            chef: params[:chef],
+                            ingredients: params[:ingredients],
+                            directions: params[:directions]    
+                            )
+    recipe.save
+  end
+
+
+
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  get '/one_recipe_url' => 'recipes#one_recipe_action'
+  get '/' => 'recipes#index'
+  get '/recipes' => 'recipes#index'
+  
+  get '/recipes/new' => 'recipes#new'
+  post '/recipes' => 'recipes#create'
+  
+  get '/recipes/:id' => 'recipes#show'
+
+  get '/recipes/:id/edit' => 'recipes#edit'
+  patch 'recipes/:id' => 'recipes#update'
 end
