0200c9d9a1229678421af3d5d8405596aa8db070
victorclee <victorchristopherlee@gmail.com>
2017-05-09 21:31:31 -0500

    added navbar, CSS font stack, Ruby syntax

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,25 @@
 class Recipe < ApplicationRecord
+  
+  def ingredient_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def friendly_updated_at
+    updated_at.strftime('%b %d, %Y')
+  end
+
+  # def friendly_prep_time
+  #   hours = prep_time / 60
+  #   minutes = prep_time % 60
+  #   time_message = ""
+  #   time_message += "#{hours} #{"Hour".pluralize(hours)}" if hours > 0
+  #   time_message += ", " if hours > 0 && minutes > 0
+  #   time_message += "#{minutes} #{"Minute".pluralize(minutes)}" if minutes > 0
+  #   time_message
+  # end
+
 end
