7182deccb588c8cbd0dc0737b32f4f4c60f88336
victorclee <victorchristopherlee@gmail.com>
2017-05-16 22:00:59 -0500

    Multiple images on show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,21 @@
 class RecipesController < ApplicationController
   def index
+    if session[:count] == nil
+      session[:count] = 0
+    end
+
+    session[:count] += 1
+    @visit_count = session[:count]
+
+
+
+
+
     @recipes = Recipe.all
+    sort_attribute = params[:sort]
+    if sort_attribute
+      @recipes = Recipe.all.order(sort_attribute)
+    end
   end
 
   def show
